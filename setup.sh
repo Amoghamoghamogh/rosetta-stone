1dfd577cee29de4d7e68e1e90d165344ccebc1e3
Amoghamoghamogh <aamogh82@gmail.com>
2025-11-09 17:20:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sudo apt install gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi gcc-powerpc-linux-gnu 
+sudo apt install gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi gcc-powerpc-linux-gnu gcc-x86-64-linux-gnu binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi binutils-powerpc-linux-gnu
+binutils-x86-64-linux-gnu
